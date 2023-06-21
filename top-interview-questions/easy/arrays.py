fac83cef06ab8506ceb8314ce9653d04fe49de0d
Phillip <phillip.besse@gmail.com>
2023-06-21 11:55:03 -0400

    add MoveZeroes solution

--- top-interview-questions/easy/arrays.py
+++ top-interview-questions/easy/arrays.py
@@ -168,3 +168,30 @@ class Solution:
             else:
                 digits[i] = 0
         return [1] + digits
+
+'''MOVE ZEROES'''
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Note that you must do this in-place without making a copy of the array.
+# Example 1:
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [0]
+
+# Constraints:
+# 1 <= nums.length <= 104
+# -231 <= nums[i] <= 231 - 1
+
+# Follow up: Could you minimize the total number of operations done?
+'''
+Do not return anything, modify nums in-place instead.
+'''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[k] = nums[k], nums[i]
+                k += 1
