bb3ee2efd7ab00feb2d237cc7a67fae9427b92a9
Phillip <phillip.besse@gmail.com>
2023-06-21 08:07:21 -0400

    add ContainsDuplicate solution

--- top-interview-questions/easy/arrays/arrays.py
+++ top-interview-questions/easy/arrays/arrays.py
@@ -68,3 +68,14 @@ class Solution:
         """
         k %= len(nums)
         nums[:] = nums[-k:] + nums[:-k]
+
+'''CONTAINS DUPLICATE'''
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        
+        return False
