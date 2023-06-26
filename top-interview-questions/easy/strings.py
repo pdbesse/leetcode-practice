bab213e1874fd5de4ed92e93f28c5e36ca9c10fe
Phillip <phillip.besse@gmail.com>
2023-06-26 08:57:50 -0400

    add ValidAnagram solution

--- top-interview-questions/easy/strings.py
+++ top-interview-questions/easy/strings.py
@@ -110,3 +110,38 @@ class Solution:
                 return i
         
         return -1
+    
+'''VALID ANAGRAM'''
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+
+# Constraints:
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+ 
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): 
+            return False
+        
+        counts = [0]*26
+        for c1,c2 in zip(s,t):
+            counts[ord(c1) - ord('a')] += 1
+            counts[ord(c2) - ord('a')] -= 1
+        
+        anagram = True
+        for num in counts:
+            if num != 0:
+                anagram = False
+        
+        return anagram
