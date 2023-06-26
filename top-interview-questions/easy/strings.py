b9d178af2e7d718da2c4126d094cddbd861a0d0b
Phillip <phillip.besse@gmail.com>
2023-06-26 09:19:43 -0400

    add Implement_strStr() solution

--- top-interview-questions/easy/strings.py
+++ top-interview-questions/easy/strings.py
@@ -268,3 +268,37 @@ class Solution:
         
         # Step 5: Return the final result
         return num
+
+'''IMPLEMENT strStr()'''
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+#     Input: haystack = "sadbutsad", needle = "sad"
+#     Output: 0
+#     Explanation: "sad" occurs at index 0 and 6.
+#     The first occurrence is at index 0, so we return 0.
+
+# Example 2:
+#     Input: haystack = "leetcode", needle = "leeto"
+#     Output: -1
+#     Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+# Constraints:
+#     1 <= haystack.length, needle.length <= 104
+#     haystack and needle consist of only lowercase English characters.
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # Time: O(n*m)
+        # Space: O(1)
+        # Edge case
+        if not needle:
+            return 0
+        
+        # Iterate through the haystack
+        for i in range(len(haystack)):
+            # Check if the substring starting at the current index matches the needle
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        
+        return -1
