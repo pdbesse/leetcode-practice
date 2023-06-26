bed66e6c04da25a91f82723e89509afb1c6b8731
Phillip <phillip.besse@gmail.com>
2023-06-26 09:01:12 -0400

    add ValidPalindome solution

--- top-interview-questions/easy/strings.py
+++ top-interview-questions/easy/strings.py
@@ -145,3 +145,44 @@ class Solution:
                 anagram = False
         
         return anagram
+
+'''VALID PALINDROME'''
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+# Example 2:
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+# Example 3:
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+# Constraints:
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ''
+
+        for c in s:
+            if c.isalnum():
+                new_s += c.lower()
+
+        start,end = 0,len(new_s)-1
+        while(start<end):
+            if new_s[start] != new_s[end]:
+                return False
+            start += 1
+            end -= 1
+        
+        return True
