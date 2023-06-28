379a2d7ea7e3403070d7540ce21a5d35553eba6b
Phillip <phillip.besse@gmail.com>
2023-06-28 09:12:40 -0400

    add BTLevelOrderTraversal solution

--- top-interview-questions/easy/trees.py
+++ top-interview-questions/easy/trees.py
@@ -146,3 +146,57 @@ class Solution:
             queue.put(right.left)
         
         return True
+
+'''BINARY TREE LEVEL ORDER TRAVERSAL'''
+# Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+# Example 1:
+#     Input: root = [3,9,20,null,null,15,7]
+#     Output: [[3],[9,20],[15,7]]
+
+# Example 2:
+#     Input: root = [1]
+#     Output: [[1]]
+
+# Example 3:
+#     Input: root = []
+#     Output: []
+
+# Constraints:
+#     The number of nodes in the tree is in the range [0, 2000].
+#     -1000 <= Node.val <= 1000
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        out = []
+        q = Queue()
+        q.put(root)
+
+        while not q.empty():
+            level = []
+            next_level = Queue()
+
+            while not q.empty():
+                node = q.get()
+                level.append(node.val)
+
+                if node.left:
+                    next_level.put(node.left)
+                if node.right:
+                    next_level.put(node.right)
+                
+            out.append(level)
+            q = next_level
+        
+        return out
+        
