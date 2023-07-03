747c2b3f9e77787a96aca31d42817f2480c31fc1
Phillip <phillip.besse@gmail.com>
2023-07-03 10:04:45 -0400

    add CountPrimes solution

--- top-interview-questions/easy/math.py
+++ top-interview-questions/easy/math.py
@@ -34,5 +34,44 @@ class Solution:
                 out.append('Buzz')
             else:
                 out.append(str(i))
-                
+
+        return out
+    
+'''COUNT PRIMES'''
+# Given an integer n, return the number of prime numbers that are strictly less than n.
+
+# Example 1:
+#     Input: n = 10
+#     Output: 4
+#     Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+# Example 2:
+#     Input: n = 0
+#     Output: 0
+
+# Example 3:
+#     Input: n = 1
+#     Output: 0
+
+# Constraints:
+#     0 <= n <= 5 * 106
+
+# HINT: use Sieve of Eratosthenes
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        out = []
+        prime = [True for i in range(n+1)]
+        p = 2
+
+        while p*p <= n:
+            if (prime[p] == True):
+                for i in range(p*p, n+1, p):
+                    prime[i] = False
+            p += 1
+        
+        for p in range(2, n+1):
+            if prime[p]:
+                out.append[p]
+        
         return out
