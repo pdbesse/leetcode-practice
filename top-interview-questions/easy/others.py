c6445abc1b3d30bff9f57a68a321da4a8dac15e0
Phillip <phillip.besse@gmail.com>
2023-07-03 12:13:22 -0400

    add MissingNumber solution

--- top-interview-questions/easy/others.py
+++ top-interview-questions/easy/others.py
@@ -1,3 +1,5 @@
+from typing import List
+
 '''NUMBER OF 1 BITS'''
 # Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
 # Note:
@@ -170,3 +172,40 @@ class Solution:
                     return False
         
         return len(stack) == 0
+
+'''MISSING NUMBER'''
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+# Example 1:
+#     Input: nums = [3,0,1]
+#     Output: 2
+#     Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+# Example 2:
+#     Input: nums = [0,1]
+#     Output: 2
+#     Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+# Example 3:
+#     Input: nums = [9,6,4,2,3,5,7,0,1]
+#     Output: 8
+#     Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
+# Constraints:
+#     n == nums.length
+#     1 <= n <= 104
+#     0 <= nums[i] <= n
+#     All the numbers of nums are unique.
+
+# Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # Initialize missing as the last index
+        missing = len(nums)
+
+        for i, num in enumerate(nums):
+            # XOR missing with the index and the number
+            missing ^= i ^ num
+        
+        return missing
