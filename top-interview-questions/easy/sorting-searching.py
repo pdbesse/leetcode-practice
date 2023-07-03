12dc2658dc0f9217e666074eb1069ce298729be6
Phillip <phillip.besse@gmail.com>
2023-07-03 09:02:17 -0400

    add FirstBadVersion solution

--- top-interview-questions/easy/sorting-searching.py
+++ top-interview-questions/easy/sorting-searching.py
@@ -55,3 +55,43 @@ class Solution:
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
+
+'''FIRST BAD VERSION'''
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+# Example 1:
+#     Input: n = 5, bad = 4
+#     Output: 4
+#     Explanation:
+#     call isBadVersion(3) -> false
+#     call isBadVersion(5) -> true
+#     call isBadVersion(4) -> true
+#     Then 4 is the first bad version.
+
+# Example 2:
+#     Input: n = 1, bad = 1
+#     Output: 1
+
+# Constraints:
+#     <= bad <= n <= 231 - 1
+
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int) -> bool:
+    pass
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if n == 1:
+            return 1
+        p1 = 1
+        p2 = n
+
+        while p1 < p2:
+            mid = p1 + (p2 - p1) // 2
+            if isBadVersion(mid):
+                p2 = mid
+            else:
+                p1 = mid + 1
+        
+        return p1
