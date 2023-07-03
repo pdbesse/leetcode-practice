a5023eba93773dd20cec69a38385ab15a7698c8c
Phillip <phillip.besse@gmail.com>
2023-07-03 09:16:53 -0400

    add MaxSubarray solution

--- top-interview-questions/easy/dynamic-programming.py
+++ top-interview-questions/easy/dynamic-programming.py
@@ -69,3 +69,37 @@ class Solution:
             dp[i] = max(dp[i-1], prices[i] - min_price)
 
         return dp[-1]
+
+'''MAXIMUM SUBARRAY'''
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+# Example 1:
+#     Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+#     Output: 6
+#     Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Example 2:
+#     Input: nums = [1]
+#     Output: 1
+#     Explanation: The subarray [1] has the largest sum 1.
+
+# Example 3:
+#     Input: nums = [5,4,-1,7,8]
+#     Output: 23
+#     Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+# Constraints:
+#     1 <= nums.length <= 105
+#     -104 <= nums[i] <= 104
+
+# Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum
