30beb2a1f65eabedba90d6b82235e3a01c887499
Phillip <phillip.besse@gmail.com>
2023-07-03 09:47:24 -0400

    add MinStack solution

--- top-interview-questions/easy/design.py
+++ top-interview-questions/easy/design.py
@@ -49,3 +49,71 @@ class Solution:
             self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
         
         return self.nums
+
+'''MIN STACK'''
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+# Implement the MinStack class:
+#     MinStack() initializes the stack object.
+#     void push(int val) pushes the element val onto the stack.
+#     void pop() removes the element on the top of the stack.
+#     int top() gets the top element of the stack.
+#     int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+# Example 1:
+# Input
+#     ["MinStack","push","push","push","getMin","pop","top","getMin"]
+#     [[],[-2],[0],[-3],[],[],[],[]]
+# Output
+#     [null,null,null,null,-3,null,0,-2]
+# Explanation
+#     MinStack minStack = new MinStack();
+#     minStack.push(-2);
+#     minStack.push(0);
+#     minStack.push(-3);
+#     minStack.getMin(); // return -3
+#     minStack.pop();
+#     minStack.top();    // return 0
+#     minStack.getMin(); // return -2
+
+# Constraints:
+#     -231 <= val <= 231 - 1
+#     Methods pop, top and getMin operations will always be called on non-empty stacks.
+#     At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+# Your MinStack object will be instantiated and called as such:
+    # obj = MinStack()
+    # obj.push(val)
+    # obj.pop()
+    # param_3 = obj.top()
+    # param_4 = obj.getMin()
+
+class MinStack:
+    def __init__(self):
+        # stack to store elements
+        self.stack = [] 
+        # stack to store minimum value
+        self.min_stack = [] 
+
+    def push(self, val: int) -> None:
+        # push element onto stack
+        self.stack.append(val)
+        # if the min stack is empty or the new value is less than or equal to the current minimum,
+        # push the new value onto the min stack
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        # if element being popped is the current minimum, pop it from the min stack as well
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        # pop element from stack
+        self.stack.pop()
+
+    def top(self) -> int:
+        # return top element of the stack
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        # return the current minimum value
+        return self.min_stack[-1]
