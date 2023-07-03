c5c36173b0db02572566e8f05046e7ba182a8434
Phillip <phillip.besse@gmail.com>
2023-07-03 09:12:54 -0400

    add BestTimeToBuyAndSellAStock solution

--- top-interview-questions/easy/dynamic-programming.py
+++ top-interview-questions/easy/dynamic-programming.py
@@ -35,3 +35,37 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
         
         return dp[n]
+
+'''BEST TIME TO BUY AND SELL A STOCK'''
+#     You are given an array prices where prices[i] is the price of a given stock on the ith day.
+#     You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+#     Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+# Example 1:
+#     Input: prices = [7,1,5,3,6,4]
+#     Output: 5
+#     Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+#     Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+# Example 2:
+#     Input: prices = [7,6,4,3,1]
+#     Output: 0
+#     Explanation: In this case, no transactions are done and the max profit = 0.
+
+# Constraints:
+#     prices.length <= 105
+#     0 <= prices[i] <= 104
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        
+        dp = [0]*len(prices)
+        min_price = prices[0]
+
+        for i in range(1,len(prices)):
+            min_price = min(min_price, prices[i])
+            dp[i] = max(dp[i-1], prices[i] - min_price)
+
+        return dp[-1]
