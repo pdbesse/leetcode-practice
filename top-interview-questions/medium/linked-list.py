9cf55984053892ca917d3eeecba98ce4509fdc76
Phillip <phillip.besse@gmail.com>
2023-07-05 12:03:25 -0400

    add OddEvenLinkedList solution

--- top-interview-questions/medium/linked-list.py
+++ top-interview-questions/medium/linked-list.py
@@ -56,6 +56,45 @@ class Solution:
                 head = node
                 
             prev = node
-            
+
         # Return the head of the resulting linked list
-        return head  
+        return head 
+
+'''ODD EVEN LINKED LIST'''
+# Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+# The first node is considered odd, and the second node is even, and so on.
+# Note that the relative order inside both the even and odd groups should remain as it was in the input.
+# You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+# Example 1:
+#     Input: head = [1,2,3,4,5]
+#     Output: [1,3,5,2,4]
+
+# Example 2:
+#     Input: head = [2,1,3,5,6,4,7]
+#     Output: [2,3,6,7,1,5,4]
+
+# Constraints:
+#     The number of nodes in the linked list is in the range [0, 104].
+#     -106 <= Node.val <= 106
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        odd_head = head
+        even_head = head.next
+        odd_ptr = odd_head
+        even_ptr = even_head
+        
+        while even_ptr and even_ptr.next:
+            odd_ptr.next = even_ptr.next
+            odd_ptr = odd_ptr.next
+            even_ptr.next = odd_ptr.next
+            even_ptr = even_ptr.next
+        
+        odd_ptr.next = even_head
+        even_ptr.next = None
+        
+        return odd_head
