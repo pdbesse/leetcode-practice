d9b817d18a476b85218b468278ada9237dc7a1e5
Phillip <phillip.besse@gmail.com>
2023-07-05 08:23:54 -0400

    add LongestSubstringWithoutRepeatingChars solution

--- top-interview-questions/medium/arrays-strings.py
+++ top-interview-questions/medium/arrays-strings.py
@@ -174,3 +174,54 @@ class Solution:
                 anagram_groups[sorted_word] = [word]
         
         return list(anagram_groups.values())
+
+'''LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS'''
+# Given a string s, find the length of the longest substring without repeating characters.
+
+# Example 1:
+    # Input: s = "abcabcbb"
+    # Output: 3
+    # Explanation: The answer is "abc", with the length of 3.
+
+# Example 2:
+#     Input: s = "bbbbb"
+#     Output: 1
+#     Explanation: The answer is "b", with the length of 1.
+
+# Example 3:
+#     Input: s = "pwwkew"
+#     Output: 3
+#     Explanation: The answer is "wke", with the length of 3.
+#     Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+# Constraints:
+#     0 <= s.length <= 5 * 104
+#     s consists of English letters, digits, symbols and spaces.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+
+        # Set to store characters in current window
+        window = set()
+
+        # Initialize start and end pointers
+        start = 0
+        end = 0
+
+        # Initialize maximum length
+        max_length = 0
+
+        # Move the end pointer through the string
+        while end < n:
+            # If the character at the end pointer is not in the set, add it and move the pointer
+            if s[end] not in window:
+                window.add(s[end])
+                end += 1
+                max_length = max(max_length, end - start)
+            # If the character at the end pointer is in the set, remove the character at the start pointer and move it
+            else:
+                window.remove(s[start])
+                start += 1
+
+        return max_length
