f0e69ef2cec8332011d493b1f3b5a566a39860ae
Phillip <phillip.besse@gmail.com>
2023-07-06 12:32:07 -0400

    add ConstructBinaryTreeFromPreorderAndInorderTraversal solution

--- top-interview-questions/medium/trees-graphs.py
+++ top-interview-questions/medium/trees-graphs.py
@@ -95,3 +95,41 @@ class Solution:
             level += 1
 
         return traversal
+
+'''CONSTRUCT BINARY TREE FROM PREORDER AND INORDER TRAVERSAL'''
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+# Example 1:
+#     Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+#     Output: [3,9,20,null,null,15,7]
+
+# Example 2:
+#     Input: preorder = [-1], inorder = [-1]
+#     Output: [-1]
+
+# Constraints:
+#     1 <= preorder.length <= 3000
+#     inorder.length == preorder.length
+#     -3000 <= preorder[i], inorder[i] <= 3000
+#     preorder and inorder consist of unique values.
+#     Each value of inorder also appears in preorder.
+#     preorder is guaranteed to be the preorder traversal of the tree.
+#     inorder is guaranteed to be the inorder traversal of the tree.
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        
+        # Create the root node using the first element of the preorder traversal
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        
+        # Find the index of the root value in the inorder traversal
+        root_index = inorder.index(root_val)
+        
+        # Recursively build the left and right subtrees
+        root.left = self.buildTree(preorder[1:root_index+1], inorder[:root_index])
+        root.right = self.buildTree(preorder[root_index+1:], inorder[root_index+1:])
+        
+        return root
