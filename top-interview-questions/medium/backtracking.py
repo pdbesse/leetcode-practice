1d12d9a5b5447ce544b2f3263e79a633fa15537c
Phillip <phillip.besse@gmail.com>
2023-07-06 15:22:28 -0400

    add WordSearch solution

--- top-interview-questions/medium/backtracking.py
+++ top-interview-questions/medium/backtracking.py
@@ -161,3 +161,60 @@ class Solution:
         backtrack(0, [])
 
         return out
+
+'''WORD SEARCH'''
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+# Example 1:
+#     Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+#     Output: true
+
+# Example 2:
+#     Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+#     Output: true
+
+# Example 3:
+#     Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+#     Output: false
+
+# Constraints:
+#     m == board.length
+#     n = board[i].length
+#     1 <= m, n <= 6
+#     1 <= word.length <= 15
+#     board and word consists of only lowercase and uppercase English letters.
+ 
+# Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def backtrack(row, col, word_idx):
+            if word_idx == len(word):
+                return True
+
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != word[word_idx]:
+                return False
+
+            # Mark the current cell as visited
+            temp = board[row][col]
+            board[row][col] = ''
+
+            # Explore the neighboring cells
+            found = (backtrack(row - 1, col, word_idx + 1) or
+                     backtrack(row + 1, col, word_idx + 1) or
+                     backtrack(row, col - 1, word_idx + 1) or
+                     backtrack(row, col + 1, word_idx + 1))
+
+            # Restore the cell's value
+            board[row][col] = temp
+
+            return found
+
+        # Iterate over each cell in the board
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i, j, 0):
+                    return True
+
+        return False
