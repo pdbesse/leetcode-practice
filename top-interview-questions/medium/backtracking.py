7bc97102d4df055517a881833cff545b0ba7d331
Phillip <phillip.besse@gmail.com>
2023-07-06 15:10:01 -0400

    add Permutations solution

--- top-interview-questions/medium/backtracking.py
+++ top-interview-questions/medium/backtracking.py
@@ -82,3 +82,45 @@ class Solution:
         # If there are more right parentheses left, add one
         if right_count > left_count:
             self.helper(left_count, right_count - 1, current + ')', lst)
+
+'''PERMUTATIONS'''
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+# Example 1:
+#     Input: nums = [1,2,3]
+#     Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+# Example 2:
+#     Input: nums = [0,1]
+#     Output: [[0,1],[1,0]]
+
+# Example 3:
+#     Input: nums = [1]
+#     Output: [[1]]
+
+# Constraints:
+#     1 <= nums.length <= 6
+#     -10 <= nums[i] <= 10
+#     All the integers of nums are unique.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        out = []
+        def backtrack(first=0):
+            if first == n:
+                out.append(nums[:])
+            for i in range(first, n):
+                # Swap current element with first element
+                nums[first], nums[i] = nums[i], nums[first]
+                # Generate all permutations starting from next element
+                backtrack(first + 1)
+                # Restore original list state
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+
+        backtrack()
+
+        return out
