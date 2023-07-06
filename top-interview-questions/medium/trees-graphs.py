f37c1b816d7fef1d13d28fd63ef29c07818f117d
Phillip <phillip.besse@gmail.com>
2023-07-06 13:01:41 -0400

    add NumerOfIslands solution

--- top-interview-questions/medium/trees-graphs.py
+++ top-interview-questions/medium/trees-graphs.py
@@ -238,3 +238,65 @@ class Solution:
             return
 
         self.inorderTraversal(node.right, k)
+
+'''NUMBER OF ISLANDS'''
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+# Example 1:
+#     Input: grid = [
+#     ["1","1","1","1","0"],
+#     ["1","1","0","1","0"],
+#     ["1","1","0","0","0"],
+#     ["0","0","0","0","0"]
+#     ]
+#     Output: 1
+
+# Example 2:
+#     Input: grid = [
+#     ["1","1","0","0","0"],
+#     ["1","1","0","0","0"],
+#     ["0","0","1","0","0"],
+#     ["0","0","0","1","1"]
+#     ]
+#     Output: 3
+
+# Constraints:
+#     m == grid.length
+#     n == grid[i].length
+#     1 <= m, n <= 300
+#     grid[i][j] is '0' or '1'.
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        count = 0
+
+        def DFS(row,col):
+            if row < 0 or row >= m or col < 0 or col >= n or grid[row][col] != '1':
+                return
+            
+            # Mark current cell as visited
+            grid[row][col] = '0'
+
+            # Explore neighbor cells
+            # Up
+            DFS(row-1, col)
+            # Down
+            DFS(row+1, col)
+            # Left
+            DFS(row, col-1)
+            # Right
+            DFS(row, col+1)
+
+        # Iterate over each cell in grid
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    count += 1
+                    DFS(i,j)
+        
+        return count
