2d3f6e882eaf589dee6861a99f740b984a54cd8e
Phillip <phillip.besse@gmail.com>
2023-07-06 12:54:15 -0400

    add KthSmallestElementInABST solution

--- top-interview-questions/medium/trees-graphs.py
+++ top-interview-questions/medium/trees-graphs.py
@@ -195,3 +195,46 @@ class Solution:
         
         return root
                 
+'''KTH SMALLEST ELEMENT IN A BST'''
+# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+# Example 1:
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+
+# Example 2:
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
+
+# Constraints:
+#     The number of nodes in the tree is n.
+#     1 <= k <= n <= 104
+#     0 <= Node.val <= 104
+ 
+# Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+# Hints:
+#     - Try to utilize the property of a BST.
+#     - Try in-order traversal. (Credits to @chan13)
+#     - What if you could modify the BST node's structure?
+#     - The optimal runtime complexity is O(height of BST).
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.count = 0
+        self.result = None
+        self.inorderTraversal(root, k)
+        return self.result
+
+    def inorderTraversal(self, node: Optional[TreeNode], k: int) -> None:
+        if node is None:
+            return
+
+        self.inorderTraversal(node.left, k)
+
+        self.count += 1
+        if self.count == k:
+            self.result = node.val
+            return
+
+        self.inorderTraversal(node.right, k)
