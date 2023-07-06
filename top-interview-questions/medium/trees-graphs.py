0404b53936ba6901148749c5a0e0c5a581a5d362
Phillip <phillip.besse@gmail.com>
2023-07-06 12:38:03 -0400

    add PopulatingNextRightPointersInEachNode solution

--- top-interview-questions/medium/trees-graphs.py
+++ top-interview-questions/medium/trees-graphs.py
@@ -8,6 +8,15 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
 '''BINARY TREE INORDER TRAVERSAL'''
 # Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
@@ -133,3 +142,56 @@ class Solution:
         root.right = self.buildTree(preorder[root_index+1:], inorder[root_index+1:])
         
         return root
+
+'''POPULATING NEXT RIGHT POINTERS IN EACH NODE'''
+# You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. 
+# The binary tree has the following definition:
+#     struct Node {
+#     int val;
+#     Node *left;
+#     Node *right;
+#     Node *next;
+#     }
+# Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+# Initially, all next pointers are set to NULL.
+
+# Example 1:
+#     Input: root = [1,2,3,4,5,6,7]
+#     Output: [1,#,2,3,#,4,5,6,7,#]
+#     Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+# Example 2:
+#     Input: root = []
+#     Output: []
+
+# Constraints:
+#     The number of nodes in the tree is in the range [0, 212 - 1].
+#     -1000 <= Node.val <= 1000
+
+# Follow-up:
+#     You may only use constant extra space.
+#     The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        leftmost = root
+        # continue to last lever
+        while leftmost.left:
+            head = leftmost
+            while head:
+                # Connect children of current node
+                head.left.next = head.right
+                # Connect right child of current node to left child of next node
+                if head.next:
+                    head.right.next = head.next.left
+                # Move to next node on current level
+                head = head.next
+
+            # Move to next level
+            leftmost = leftmost.left
+        
+        return root
+                
