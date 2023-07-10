243f08ed88782d37c53aa2c5971f381248edf23f
Phillip <phillip.besse@gmail.com>
2023-07-10 09:32:24 -0400

    add TopKFrequentElements solution

--- top-interview-questions/medium/sorting-searching.py
+++ top-interview-questions/medium/sorting-searching.py
@@ -48,3 +48,37 @@ class Solution:
             else:
                 current += 1
 
+'''TOP K FREQUENT ELEMENTS'''
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+# Example 1:
+#     Input: nums = [1,1,1,2,2,3], k = 2
+#     Output: [1,2]
+
+# Example 2:
+#     Input: nus = [1], k = 1
+#     Output: [1]
+
+# Constraints:
+#     1 <= nums.length <= 105
+#     -104 <= nums[i] <= 104
+#     k is in the range [1, the number of unique elements in the array].
+#     It is guaranteed that the answer is unique.
+
+# Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Create a dictionary to store the frequency of each element
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+        
+        # Create a list of tuples from the dictionary and sort it
+        sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+        
+        # Return the first k elements of the sorted list
+        return [sorted_freq[i][0] for i in range(k)]
