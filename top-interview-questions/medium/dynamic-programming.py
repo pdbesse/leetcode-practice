078e5f15191ae834849d9ad150bff614907bc5c6
Phillip <phillip.besse@gmail.com>
2023-07-13 08:27:05 -0400

    add new UniquePaths solution

--- top-interview-questions/medium/dynamic-programming.py
+++ top-interview-questions/medium/dynamic-programming.py
@@ -58,17 +58,13 @@ class Solution:
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0] * n for _ in range(m)]
-        dp[m-1][n-1] = 1
+        dp[0][0] = 1
 
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if i == m - 1 and j == n - 1:
-                    continue
-                if i == m - 1:
-                    dp[i][j] = dp[i][j + 1]
-                elif j == n - 1:
-                    dp[i][j] = dp[i + 1][j]
-                else:
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
-            
-        return dp[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+
+        return dp[m-1][n-1]
