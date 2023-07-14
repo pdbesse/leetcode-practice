661010dbd3657ce6290a5fa728a9c53e1a78b752
Phillip <phillip.besse@gmail.com>
2023-07-14 09:35:24 -0400

    add CoinChange solution

--- top-interview-questions/medium/dynamic-programming.py
+++ top-interview-questions/medium/dynamic-programming.py
@@ -68,3 +68,47 @@ class Solution:
                     dp[i][j] += dp[i][j-1]
 
         return dp[m-1][n-1]
+    
+'''COIN CHANGE'''
+# You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+# Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+# You may assume that you have an infinite number of each kind of coin.
+
+# Example 1:
+#     Input: coins = [1,2,5], amount = 11
+#     Output: 3
+#     Explanation: 11 = 5 + 5 + 1
+
+# Example 2:
+#     Input: coins = [2], amount = 3
+#     Output: -1
+
+# Example 3:
+#     Input: coins = [1], amount = 0
+#     Output: 0
+
+# Constraints:
+#     1 <= coins.length <= 12
+#     1 <= coins[i] <= 231 - 1
+#     0 <= amount <= 104
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [[float("inf")]*(amount+1) for _ in range(len(coins))]
+        dp[0][0] = 0
+        firstCoin = coins[0]
+        for i in range(amount+1):
+            if i >= firstCoin:
+                dp[0][i] = dp[0][i-firstCoin] + 1
+
+        for i in range(1,len(coins)):
+            for j in range(amount+1):
+                if j >= coins[i]:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i]]+1)
+                else:
+                    dp[i][j] = dp[i-1][j]
+        # print(dp)
+        if dp[-1][-1] != float("inf"):
+            return dp[-1][-1]
+        else:
+            return -1
