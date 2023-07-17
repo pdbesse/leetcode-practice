435045e64472ab8f0129e1f26e385aff5bb96783
Phillip <phillip.besse@gmail.com>
2023-07-17 08:31:34 -0400

    add DivideTwoIntegers solution

--- top-interview-questions/medium/math.py
+++ top-interview-questions/medium/math.py
@@ -198,3 +198,62 @@ class Solution:
                 left = mid + 1
 
         return right
+
+'''DIVIDE TWO INTEGERS'''
+# Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+# The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+# Return the quotient after dividing dividend by divisor.
+# Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+# Example 1:
+# Input: dividend = 10, divisor = 3
+# Output: 3
+# Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+# Example 2:
+# Input: dividend = 7, divisor = -3
+# Output: -2
+# Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+
+# Constraints:
+# -231 <= dividend, divisor <= 231 - 1
+# divisor != 0
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        # Handle special cases
+        if divisor == 0:
+            raise ZeroDivisionError("Divisor cannot be zero.")
+        if dividend == 0:
+            return 0
+
+        # Determine the sign of the quotient
+        sign = (dividend > 0) ^ (divisor > 0)
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        quotient = 0
+        while dividend >= divisor:
+            # Find the largest multiple of the divisor that can be subtracted from the dividend
+            multiple = 1
+            temp_divisor = divisor
+            while (temp_divisor << 1) <= dividend:
+                temp_divisor <<= 1
+                multiple <<= 1
+
+            dividend -= temp_divisor
+            quotient += multiple
+
+        # Apply the sign to the quotient
+        if sign:
+            quotient = -quotient
+
+        # Handle overflow
+        max_int = 2**31 - 1
+        min_int = -2**31
+        if quotient > max_int:
+            return max_int
+        elif quotient < min_int:
+            return min_int
+        else:
+            return quotient
