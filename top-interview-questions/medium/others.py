dadd54ba95c64859df7858fd502af804b419a799
Phillip <phillip.besse@gmail.com>
2023-07-17 11:44:50 -0400

    add EvaluateReversePolishNotation solution

--- top-interview-questions/medium/others.py
+++ top-interview-questions/medium/others.py
@@ -1,3 +1,5 @@
+from typing import List
+
 '''SUM OF TWO INTEGERS'''
 # Given two integers a and b, return the sum of the two integers without using the operators + and -.
 
@@ -32,3 +34,69 @@ class Solution:
         if a & (bit_mask << 31):
             a = ~(a ^ mask)
         return a if a <= 0x7FFFFFFF else ~(a ^ mask)
+    
+'''EVALUATE REVERSE POLISH NOTATION'''
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+# Evaluate the expression. Return an integer that represents the value of the expression.
+# Note that:
+#     The valid operators are '+', '-', '*', and '/'.
+#     Each operand may be an integer or another expression.
+#     The division between two integers always truncates toward zero.
+#     There will not be any division by zero.
+#     The input represents a valid arithmetic expression in a reverse polish notation.
+#     The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+# Example 1:
+#     Input: tokens = ["2","1","+","3","*"]
+#     Output: 9
+#     Explanation: ((2 + 1) * 3) = 9
+
+# Example 2:
+#     Input: tokens = ["4","13","5","/","+"]
+#     Output: 6
+#     Explanation: (4 + (13 / 5)) = 6
+
+# Example 3:
+#     Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+#     Output: 22
+#     Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+#     = ((10 * (6 / (12 * -11))) + 17) + 5
+#     = ((10 * (6 / -132)) + 17) + 5
+#     = ((10 * 0) + 17) + 5
+#     = (0 + 17) + 5
+#     = 17 + 5
+#     = 22
+
+# Constraints:
+#     1 <= tokens.length <= 104
+#     tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operators = set(['+', '-', '*', '/'])
+
+        for token in tokens:
+            if token in operators:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+
+                result =  self.performOperation(token, operand1, operand2)
+                stack.append(result)
+            else:
+                operand = int(token)
+                stack.append(operand)
+        
+        return stack.pop()
+    
+    def performOperation(self, op, operand1, operand2):
+        if op == '+':
+            return operand1 + operand2
+        elif op == '-':
+            return operand1 - operand2
+        elif op == '*':
+            return operand1 * operand2
+        elif op == '/':
+            return int(operand1 / operand2)
+
+        return 0
