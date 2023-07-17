4dc197732da5e3c6508b2810cde5168e3ab16073
Phillip <phillip.besse@gmail.com>
2023-07-17 08:39:27 -0400

    add FractionToRecurringDecimal solution

--- top-interview-questions/medium/math.py
+++ top-interview-questions/medium/math.py
@@ -257,3 +257,61 @@ class Solution:
             return min_int
         else:
             return quotient
+
+'''FRACTION TO RECURRING DECIMAL'''
+# Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+# If the fractional part is repeating, enclose the repeating part in parentheses.
+# If multiple answers are possible, return any of them.
+# It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+
+# Example 1:
+#     Input: numerator = 1, denominator = 2
+#     Output: "0.5"
+
+# Example 2:
+#     Input: numerator = 2, denominator = 1
+#     Output: "2"
+
+# Example 3:
+#     Input: numerator = 4, denominator = 333
+#     Output: "0.(012)"
+
+# Constraints:
+#     -231 <= numerator, denominator <= 231 - 1
+#     denominator != 0
+# Hints:
+#     - No scary math, just apply elementary math knowledge. Still remember how to perform a long division?
+#     - Try a long division on 4/9, the repeating part is obvious. Now try 4/333. Do you see a pattern?
+#     - Notice that once the remainder starts repeating, so does the divided result.
+#     - Be wary of edge cases! List out as many test cases as you can think of and test your code thoroughly.
+
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator == 0:
+            return '0'
+        
+        result = ''
+        if numerator * denominator < 0:
+            result += '-'
+        numerator, denominator = abs(numerator), abs(denominator)
+        result += str(numerator // denominator)
+
+        remainder = numerator % denominator
+        if remainder == 0:
+            return result
+        
+        result += '.'
+
+        remainders = {}
+        index = len(result)
+        while remainder != 0:
+            if remainder in remainders:
+                result = result[:remainders[remainder]] + '(' + result[remainders[remainder]:] + ')'
+                break
+            remainders[remainder] = index
+            digit = remainder * 10 // denominator
+            result += str(digit)
+            remainder = remainder * 10 % denominator
+            index += 1
+        
+        return result
