96e04c3b50988ebd736caf770aa76774fd31989c
Phillip <phillip.besse@gmail.com>
2023-07-18 08:09:38 -0400

    add ContainerWithMostWater solution

--- top-interview-questions/hard/arrays-strings.py
+++ top-interview-questions/hard/arrays-strings.py
@@ -139,3 +139,46 @@ class Solution:
                     result += dict[target]
         
         return result
+
+'''CONTAINER WITH MOST WATER'''
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+# Return the maximum amount of water a container can store.
+# Notice that you may not slant the container.
+
+# Example 1:
+#     Input: height = [1,8,6,2,5,4,8,3,7]
+#     Output: 49
+#     Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+# Example 2:
+#     Input: height = [1,1]
+#     Output: 1
+
+# Constraints:
+#     n == height.length
+#     2 <= n <= 105
+#     0 <= height[i] <= 104
+# Hints:
+#     - If you simulate the problem, it will be O(n^2) which is not efficient.
+#     - Try to use two-pointers. Set one pointer to the left and one to the right of the array. Always move the pointer that points to the lower line.
+#     - How can you calculate the amount of water at each step?
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            # Calculate area between pointers
+            area = min(height[left], height[right]) * (right - left)
+            # Update max_area if current area is greater
+            max_area = max(max_area, area)
+            # Move lowest line pointer
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
