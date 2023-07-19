d0309956a97a326fc2985ce46d516e2e73d9ae9c
Phillip <phillip.besse@gmail.com>
2023-07-19 13:28:15 -0400

    add MajorityElement solution

--- top-interview-questions/medium/others.py
+++ top-interview-questions/medium/others.py
@@ -100,3 +100,37 @@ class Solution:
             return int(operand1 / operand2)
 
         return 0
+
+'''MAJORITY ELEMENT'''
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+# Example 1:
+#     Input: nums = [3,2,3]
+#     Output: 3
+
+# Example 2:
+#     Input: nums = [2,2,1,1,1,2,2]
+#     Output: 2
+
+# Constraints:
+#     n == nums.length
+#     1 <= n <= 5 * 104
+#     -109 <= nums[i] <= 109
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
