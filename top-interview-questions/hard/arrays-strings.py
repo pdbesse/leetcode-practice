399d8b3e2ef9282c9f4e6bd9d7a61cb358677382
Phillip <phillip.besse@gmail.com>
2023-07-20 08:22:38 -0400

    add GameOfLife solution)

--- top-interview-questions/hard/arrays-strings.py
+++ top-interview-questions/hard/arrays-strings.py
@@ -182,3 +182,68 @@ class Solution:
                 right -= 1
         
         return max_area
+    
+'''GAME OF LIFE'''
+# According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+# The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+# Any live cell with fewer than two live neighbors dies as if caused by under-population.
+# Any live cell with two or three live neighbors lives on to the next generation.
+# Any live cell with more than three live neighbors dies, as if by over-population.
+# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+# The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+
+# Example 1:
+    # Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+    # Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+
+# Example 2:
+    # Input: board = [[1,1],[1,0]]
+    # Output: [[1,1],[1,1]]
+
+# Constraints:
+#     m == board.length
+#     n == board[i].length
+#     1 <= m, n <= 25
+#     board[i][j] is 0 or 1.
+
+# Follow up:
+    # Could you solve it in-place? Remember that the board needs to be updated simultaneously: You cannot update some cells first and then use their updated values to update other cells.
+    # In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches upon the border of the array (i.e., live cells reach the border). How would you address these problems?
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = len(board)
+        cols = len(board[0])
+
+        # Define a helper function to count live neighbors
+        def count_live_neighbors(row: int, col: int) -> int:
+            count = 0
+            directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+            for dr, dc in directions:
+                r = row + dr
+                c = col + dc
+                if 0 <= r < rows and 0 <= c < cols and abs(board[r][c]) == 1:
+                    count += 1
+            return count
+
+        # Update the state of the board based on the rules of the Game of Life
+        for r in range(rows):
+            for c in range(cols):
+                live_neighbors = count_live_neighbors(r, c)
+                if board[r][c] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[r][c] = -1  # Mark live cell that will die as -1
+                elif board[r][c] == 0 and live_neighbors == 3:
+                    board[r][c] = 2  # Mark dead cell that will become live as 2
+
+        # Restore the marked cells to their correct values
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == -1:
+                    board[r][c] = 0
+                elif board[r][c] == 2:
+                    board[r][c] = 1
