808bf67a7b4af1d870483eabf5186001dcd3ae1e
pdbesse <phillip.besse@gmail.com>
2023-07-26 11:14:12 -0400

    add MinimumWindowSubstring solution

--- top-interview-questions/hard/arrays-strings.py
+++ top-interview-questions/hard/arrays-strings.py
@@ -515,3 +515,74 @@ class Solution:
                 result.append(nums[window[0]])
 
         return result
+    
+'''MINIMUM WINDOW SUBSTRING'''
+# Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+# The testcases will be generated such that the answer is unique.
+
+# Example 1:
+    # Input: s = "ADOBECODEBANC", t = "ABC"
+    # Output: "BANC"
+    # Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+# Example 2:
+    # Input: s = "a", t = "a"
+    # Output: "a"
+    # Explanation: The entire string s is the minimum window.
+
+# Example 3:
+    # Input: s = "a", t = "aa"
+    # Output: ""
+    # Explanation: Both 'a's from t must be included in the window.
+    # Since the largest window of s only has one 'a', return empty string.
+
+# Constraints:
+#     m == s.length
+#     n == t.length
+#     1 <= m, n <= 105
+#     s and t consist of uppercase and lowercase English letters.
+
+# Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+# Hints: 
+#     - Use two pointers to create a window of letters in s, which would have all the characters from t.
+#     - Expand the right pointer until all the characters of t are covered. 
+#     - Once all the characters are covered, move the left pointer and ensure that all the characters are still covered to minimize the subarray size.
+#     - Continue expanding the right and left pointers until you reach the end of s.
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Initialize two dictionaries to store the frequency of characters in strings s and t
+        char_count_s = {}
+        char_count_t = {}
+        for char in t:
+            char_count_t[char] = char_count_t.get(char, 0) + 1
+        
+        # Function to check if all characters of t are covered in the current window
+        def is_window_valid():
+            for char, count in char_count_t.items():
+                if char_count_s.get(char, 0) < count:
+                    return False
+            return True
+        
+        left, right = 0, 0
+        min_window = float('inf')  # Initialize with a large value
+        result = ""
+        
+        while right < len(s):
+            # Expand the window to the right
+            char_count_s[s[right]] = char_count_s.get(s[right], 0) + 1
+            
+            # Shrink the window from the left if it's valid and try to minimize the size
+            while is_window_valid() and left <= right:
+                window_size = right - left + 1
+                if window_size < min_window:
+                    min_window = window_size
+                    result = s[left:right+1]
+                
+                char_count_s[s[left]] -= 1
+                left += 1
+            
+            right += 1
+        
+        return result
