5f4dfda4d5881be99146a11a7a9cce52713263fb
Phillip <phillip.besse@gmail.com>
2023-07-28 15:26:24 -0400

    add LowestCommonAncestorInABinaryTree solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -1,6 +1,13 @@
 from typing import List
 from collections import defaultdict, deque
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 '''WORD LADDER'''
 #     A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
 #     Every adjacent pair of words differs by a single letter.
@@ -127,3 +134,45 @@ class Solution:
                     board[row][col] = 'X'
                 elif board[row][col] == 'M':
                     board[row][col] = 'O'
+
+'''LOWEST COMMON ANCESTOR OF A BINARY TREE'''
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+# Example 1:
+#     Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+#     Output: 3
+#     Explanation: The LCA of nodes 5 and 1 is 3.
+
+# Example 2:
+#     Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+#     Output: 5
+#     Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+# Example 3:
+#     Input: root = [1,2], p = 1, q = 2
+#     Output: 1
+
+# Constraints:
+#     The number of nodes in the tree is in the range [2, 105].
+#     -109 <= Node.val <= 109
+#     All Node.val are unique.
+#     p != q
+#     p and q will exist in the tree.
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Base cases
+        if not root or root == p or root == q:
+            return root
+        
+        # Recursively find LCA in left and right subtrees
+        left_lca = self.lowestCommonAncestor(root.left, p, q)
+        right_lca = self.lowestCommonAncestor(root.right, p, q)
+
+        # If both nodes are in different subtrees, current node is LCA
+        if left_lca and right_lca:
+            return root
+        
+        # If either node is found in sutree, return that node as LCA
+        return left_lca if left_lca else right_lca
