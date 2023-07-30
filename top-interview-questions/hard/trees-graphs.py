f71c627319dddf3e65f4fc6127bcab540e4fc14a
Phillip <phillip.besse@gmail.com>
2023-07-30 19:44:22 -0400

    add LongestIncreasingPathInAMatrix solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -222,5 +222,64 @@ class Solution:
 
         # Initialize the global maximum path sum
         self.max_sum = float('-inf')  
+        
         maxPathSumHelper(root)
+
         return self.max_sum
+    
+
+
+
+
+'''LONGEST INCREASING PATH IN A MATRIX'''
+# Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+# From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+
+# Example 1:
+# Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+# Output: 4
+# Explanation: The longest increasing path is [1, 2, 6, 9].
+
+# Example 2:
+# Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
+# Output: 4
+# Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+
+# Example 3:
+# Input: matrix = [[1]]
+# Output: 1
+
+# Constraints:
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 200
+# 0 <= matrix[i][j] <= 231 - 1
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        if not matrix:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        memo = [[0] * n for _ in range(m)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        def dfs(row, col):
+            if memo[row][col] != 0:
+                return memo[row][col]
+
+            max_path = 1
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+                if 0 <= new_row < m and 0 <= new_col < n and matrix[new_row][new_col] > matrix[row][col]:
+                    max_path = max(max_path, 1 + dfs(new_row, new_col))
+
+            memo[row][col] = max_path
+            return max_path
+
+        longest_path = 0
+        for i in range(m):
+            for j in range(n):
+                longest_path = max(longest_path, dfs(i, j))
+
+        return longest_path
