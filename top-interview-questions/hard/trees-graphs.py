b554b8a2727edb04c5d6539a96c36797cabc62cf
Phillip <phillip.besse@gmail.com>
2023-07-31 08:14:45 -0400

    add FriendCircles solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from collections import defaultdict, deque
+from queue import Queue
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -227,9 +228,46 @@ class Solution:
 
         return self.max_sum
     
+'''FRIEND CIRCLES'''
+# There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+# A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+# You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+# Return the total number of provinces.
 
+# Example 1:
+#     Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+#     Output: 2
 
+# Example 2:
+#     Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+#     Output: 3
 
+# Constraints:
+#     1 <= n <= 200
+#     n == isConnected.length
+#     n == isConnected[i].length
+#     isConnected[i][j] is 1 or 0.
+#     isConnected[i][i] == 1
+#     isConnected[i][j] == isConnected[j][i]
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = set()
+        provinces = 0
+
+        def dfs(i):
+            for j in range(n):
+                if isConnected[i][j] == 1 and j not in visited:
+                    visited.add(j)
+                    dfs(j)
+        
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                provinces += 1
+
+        return provinces
 
 '''LONGEST INCREASING PATH IN A MATRIX'''
 # Given an m x n integers matrix, return the length of the longest increasing path in matrix.
