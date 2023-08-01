cfaa13292289f0d6cf15c3dfd08f23094b54974a
Phillip <phillip.besse@gmail.com>
2023-08-01 08:04:29 -0400

    add MIssingRanges solution

--- top-interview-questions/medium/arrays-strings.py
+++ top-interview-questions/medium/arrays-strings.py
@@ -366,3 +366,51 @@ class Solution:
 
         result += str(count) + prev[-1]
         return result
+
+'''MISSING RANGES'''
+# You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive range.
+# A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+# Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
+
+# Example 1:
+#     Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+#     Output: [[2,2],[4,49],[51,74],[76,99]]
+#     Explanation: The ranges are:
+#     [2,2]
+#     [4,49]
+#     [51,74]
+#     [76,99]
+
+# Example 2:
+#     Input: nums = [-1], lower = -1, upper = -1
+#     Output: []
+#     Explanation: There are no missing ranges since there are no missing numbers.
+
+# Constraints:
+#     -109 <= lower <= upper <= 109
+#     0 <= nums.length <= 100
+#     lower <= nums[i] <= upper
+#     All the values of nums are unique.
+
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        missing_ranges = []
+        start = lower
+        
+        for num in nums:
+            # Check if there is a gap between start and the current number
+            if num > start:
+                end = num - 1
+                self.add_range(missing_ranges, start, end)
+            
+            # Move the start to the next number
+            start = num + 1
+        
+        # Check for missing range after the last element in the list
+        if start <= upper:
+            self.add_range(missing_ranges, start, upper)
+        
+        return missing_ranges
+    
+    def add_range(self, missing_ranges: List[List[int]], start: int, end: int) -> None:
+            missing_ranges.append([start, end])
