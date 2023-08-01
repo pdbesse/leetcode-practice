3338d1cdbbd707917f4ad0b16a85d1a7af1e1937
Phillip <phillip.besse@gmail.com>
2023-08-01 08:16:19 -0400

    add InorderSuccessorOfABST solution

--- top-interview-questions/medium/trees-graphs.py
+++ top-interview-questions/medium/trees-graphs.py
@@ -239,6 +239,39 @@ class Solution:
 
         self.inorderTraversal(node.right, k)
 
+'''INORDER SUCCESSOR OF BST'''
+# Given the root of a binary search tree and a node p in it, return the in-order successor of that node in the BST. If the given node has no in-order successor in the tree, return null.
+# The successor of a node p is the node with the smallest key greater than p.val.
+
+# Example 1:
+#     Input: root = [2,1,3], p = 1
+#     Output: 2
+#     Explanation: 1's in-order successor node is 2. Note that both p and the return value is of TreeNode type.
+
+# Example 2:
+#     Input: root = [5,3,6,2,4,null,null,1], p = 6
+#     Output: null
+#     Explanation: There is no in-order successor of the current node, so the answer is null.
+
+# Constraints:
+#     The number of nodes in the tree is in the range [1, 104].
+#     -105 <= Node.val <= 105
+#     All Nodes will have unique values.
+
+class Solution:
+    def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        successor = None
+
+        while root:
+            if root.val > p.val:
+                successor = root
+                root = root.left
+            else:
+                root = root.right
+
+        return successor
+                
+
 '''NUMBER OF ISLANDS'''
 # Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 # An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
