e59fe9a70b302ea62dc22ba09723197bd2d5d4e7
Phillip <phillip.besse@gmail.com>
2023-08-02 08:24:49 -0400

    add MinimumRoomsII solution

--- top-interview-questions/medium/sorting-searching.py
+++ top-interview-questions/medium/sorting-searching.py
@@ -288,6 +288,48 @@ class Solution:
 
         return -1
 
+'''MEETING ROOMS II'''
+# Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+# Example 1:
+#     Input: intervals = [[0,30],[5,10],[15,20]]
+#     Output: 2
+
+# Example 2:
+#     Input: intervals = [[7,10],[2,4]]
+#     Output: 1
+
+# Constraints:
+#     1 <= intervals.length <= 104
+#     0 <= starti < endi <= 106
+
+# Hints:
+#     - Think about how we would approach this problem in a very simplistic way. We will allocate rooms to meetings that occur earlier in the day v/s the ones that occur later on, right?
+#     - If you've figured out that we have to sort the meetings by their start time, the next thing to think about is how do we do the allocation?
+#     There are two scenarios possible here for any meeting. Either there is no meeting room available and a new one has to be allocated, or a meeting room has freed up and this meeting can take place there.
+#     - An important thing to note is that we don't really care which room gets freed up while allocating a room for the current meeting. As long as a room is free, our job is done.
+#     We already know the rooms we have allocated till now and we also know when are they due to get free because of the end times of the meetings going on in those rooms. We can simply check the room which is due to get vacated the earliest amongst all the allocated rooms.
+#     - Following up on the previous hint, we can make use of a min-heap to store the end times of the meetings in various rooms.
+#     So, every time we want to check if any room is free or not, simply check the topmost element of the min heap as that would be the room that would get free the earliest out of all the other rooms currently occupied.
+#     If the room we extracted from the top of the min heap isn't free, then no other room is. So, we can save time here and simply allocate a new room.
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # Sort intervals by start time
+        intervals.sort(key=lambda x: x[0])
+        # Initialize min heap to store end times
+        heap = []
+        for interval in intervals:
+            # If heap is empty or current interval doesn't overlap previous interval
+            if not heap or interval[0] < heap[0]:
+                heapq.heappush(heap, interval[1])
+            else:
+                # Update end time of previous interval
+                heapq.heappop(heap)
+                heapq.heappush(heap, interval[1])
+        
+        return len(heap)
+
 '''SEARCH A 2D MATRIX II'''
 # Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
 # Integers in each row are sorted in ascending from left to right.
