e9d958bee37ff3faf89990979b0626ba77b3f76d
Phillip <phillip.besse@gmail.com>
2023-08-03 10:07:03 -0400

    add CourseScheduleII solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -299,7 +299,100 @@ class Solution:
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Build adjacency list
+        adj_list = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            adj_list[prereq].append(course)
+            indegree[course] += 1
         
+        # Init queue for BFS
+        Q = deque()
+
+        # Add all courses with indegree 0 to queue
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                Q.append(course)
+        
+        # Init courses taken counter
+        courses_taken = 0
+
+        # Perform BFS
+        while Q:
+            course = Q.popleft()
+            courses_taken += 1
+
+            for next_course in adj_list[course]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    Q.append(next_course)
+        
+        # Return if all courses taken
+        return courses_taken == numCourses
+
+'''COURSE SCHEDULE II'''
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+# Example 1:
+#     Input: numCourses = 2, prerequisites = [[1,0]]
+#     Output: [0,1]
+#     Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+
+# Example 2:
+#     Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+#     Output: [0,2,1,3]
+#     Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+#     So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+
+# Example 3:
+#     Input: numCourses = 1, prerequisites = []
+#     Output: [0]
+
+# Constraints:
+#     1 <= numCourses <= 2000
+#     0 <= prerequisites.length <= numCourses * (numCourses - 1)
+#     prerequisites[i].length == 2
+#     0 <= ai, bi < numCourses
+#     ai != bi
+#     All the pairs [ai, bi] are distinct.
+
+# Hints:
+#     - This problem is equivalent to finding the topological order in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+#     - Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
+#     - Topological sort could also be done via BFS.
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Step 1: Build the adj_list and indegree array
+        adj_list = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for course, prerequisite in prerequisites:
+            adj_list[prerequisite].append(course)
+            indegree[course] += 1
+
+        # Step 2: Perform topological sort using BFS
+        Q = Queue()
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                Q.put(course)
+
+        order = []
+        while not Q.empty():
+            course = Q.get()
+            order.append(course)
+            for next_course in adj_list[course]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    Q.put(next_course)
+
+        # Step 3: Check if there is a cycle (not all courses can be taken)
+        if len(order) != numCourses:
+            return None
+
+        return order
 
 '''LONGEST INCREASING PATH IN A MATRIX'''
 # Given an m x n integers matrix, return the length of the longest increasing path in matrix.
