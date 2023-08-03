584ce1e9545acddf463a28c60a673fd83d81b4f1
Phillip <phillip.besse@gmail.com>
2023-08-03 09:52:02 -0400

    add CourseSchedule solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -268,6 +268,38 @@ class Solution:
                 provinces += 1
 
         return provinces
+    
+'''COURSE SCHEDULE'''
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return true if you can finish all courses. Otherwise, return false.
+
+# Example 1:
+#     Input: numCourses = 2, prerequisites = [[1,0]]
+#     Output: true
+#     Explanation: There are a total of 2 courses to take. 
+#     To take course 1 you should have finished course 0. So it is possible.
+
+# Example 2:
+#     Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+#     Output: false
+#     Explanation: There are a total of 2 courses to take. 
+#     To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+# Constraints:
+#     1 <= numCourses <= 2000
+#     0 <= prerequisites.length <= 5000
+#     prerequisites[i].length == 2
+#     0 <= ai, bi < numCourses
+#     All the pairs prerequisites[i] are unique.
+# Hints: 
+#     - This problem is equivalent to finding if a cycle exists in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+#     - Topological Sort via DFS - A great tutorial explaining the basic concepts of Topological Sort.
+#     - Topological sort could also be done via BFS.
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
 
 '''LONGEST INCREASING PATH IN A MATRIX'''
 # Given an m x n integers matrix, return the length of the longest increasing path in matrix.
