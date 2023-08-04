199e859aae038048a153cf392ac4c817cb97a4cd
Phillip <phillip.besse@gmail.com>
2023-08-04 08:14:25 -0400

    add LongestSubstringWithKDistinctCharacters solution

--- top-interview-questions/hard/arrays-strings.py
+++ top-interview-questions/hard/arrays-strings.py
@@ -378,6 +378,26 @@ class Solution:
         
         return slow
     
+'''LONGEST SUBSTRING WITH AT MOST K DISTINCT CHARACTERS'''
+# Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+# Example 1:
+#     Input: s = "eceba", k = 2
+#     Output: 3
+#     Explanation: The substring is "ece" with length 3.
+    
+# Example 2:
+#     Input: s = "aa", k = 1
+#     Output: 2
+#     Explanation: The substring is "aa" with length 2.
+
+# Constraints:
+#     1 <= s.length <= 5 * 104
+#     0 <= k <= 50
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+
 '''BASIC CALCULATOR II'''
 # Given a string s which represents an expression, evaluate this expression and return its value. 
 # The integer division should truncate toward zero.
