a0e2eccaae10aa1c1566b0c4abf35b9666cad009
Phillip <phillip.besse@gmail.com>
2023-08-04 08:34:01 -0400

    add AlienDictionary solution

--- top-interview-questions/hard/trees-graphs.py
+++ top-interview-questions/hard/trees-graphs.py
@@ -446,3 +446,63 @@ class Solution:
                 longest_path = max(longest_path, dfs(i, j))
 
         return longest_path
+
+'''ALIEN DICTIONARY'''
+# There is a new alien language that uses the English alphabet. However, the order of the letters is unknown to you.
+# You are given a list of strings words from the alien language's dictionary. Now it is claimed that the strings in words are sorted lexicographically by the rules of this new language.
+# If this claim is incorrect, and the given arrangement of string in words cannot correspond to any order of letters, return "".
+# Otherwise, return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there are multiple solutions, return any of them.
+
+# Example 1:
+# Input: words = ["wrt","wrf","er","ett","rftt"]
+# Output: "wertf"
+
+# Example 2:
+# Input: words = ["z","x"]
+# Output: "zx"
+
+# Example 3:
+# Input: words = ["z","x","z"]
+# Output: ""
+# Explanation: The order is invalid, so return "".
+
+# Constraints:
+# 1 <= words.length <= 100
+# 1 <= words[i].length <= 100
+# words[i] consists of only lowercase English letters.
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # Step 1: Build the graph
+        graph = defaultdict(set)
+        in_degree = {c: 0 for word in words for c in word}
+
+        for i in range(len(words) - 1):
+            word1, word2 = words[i], words[i+1]
+            for j in range(min(len(word1), len(word2))):
+                if word1[j] != word2[j]:
+                    if word2[j] not in graph[word1[j]]:
+                        graph[word1[j]].add(word2[j])
+                        in_degree[word2[j]] += 1
+                    break
+            else:
+                # Check for invalid case where word1 is a prefix of word2
+                if len(word1) > len(word2):
+                    return ""
+
+        # Step 2: Perform topological sort
+        Q = deque([c for c in in_degree if in_degree[c] == 0])
+        result = []
+        while Q:
+            node = Q.popleft()
+            result.append(node)
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    Q.append(neighbor)
+
+        # Check if there is a cycle in the graph
+        if len(result) != len(in_degree):
+            return ""
+
+        return "".join(result)
