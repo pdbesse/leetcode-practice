c5c38f5a076f3b72359ff019034665f5edac310d
Phillip <phillip.besse@gmail.com>
2023-08-13 12:56:24 -0400

    add KthSmallestElementInASortedList solution

--- top-interview-questions/hard/sorting-searching.py
+++ top-interview-questions/hard/sorting-searching.py
@@ -46,3 +46,57 @@ class Solution:
             smaller_idx -= 1
         
         nums[:] = result  # Copy the result back to the original array
+
+'''KTH SMALLEST ELEMENT IN A SORTED LIST'''
+# Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+# Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+# You must find a solution with a memory complexity better than O(n2).
+
+# Example 1:
+#     Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+#     Output: 13
+#     Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+
+# Example 2:
+#     Input: matrix = [[-5]], k = 1
+#     Output: -5
+
+# Constraints:
+#     n == matrix.length == matrix[i].length
+#     1 <= n <= 300
+#     -109 <= matrix[i][j] <= 109
+#     All the rows and columns of matrix are guaranteed to be sorted in non-decreasing order.
+#     1 <= k <= n2
+
+# Follow up:
+#     Could you solve the problem with a constant memory (i.e., O(1) memory complexity)?
+#     Could you solve the problem in O(n) time complexity? The solution may be too advanced for an interview but you may find reading this paper fun.
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        left, right = matrix[0][0], matrix[n - 1][n - 1]
+
+        while left < right:
+            mid = left + (right - left) // 2
+            count = self.countLessEqual(matrix, mid, n)
+            
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
+    
+    def countLessEqual(self, matrix: List[List[int]], target: int, n: int) -> int:
+        count = 0
+        row, col = n - 1, 0
+        
+        while row >= 0 and col < n:
+            if matrix[row][col] <= target:
+                count += row + 1
+                col += 1
+            else:
+                row -= 1
+        
+        return count
