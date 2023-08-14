c719314896e4b7e9fc7f83727f88ec7386c01547
Phillip <phillip.besse@gmail.com>
2023-08-14 08:12:44 -0400

    add MedianOfTwoSortedArrays solution

--- top-interview-questions/hard/sorting-searching.py
+++ top-interview-questions/hard/sorting-searching.py
@@ -100,3 +100,56 @@ class Solution:
                 row -= 1
         
         return count
+    
+'''MEDIAN OF TWO SORTED ARRAYS'''
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+# The overall run time complexity should be O(log (m+n)).
+
+# Example 1:
+#     Input: nums1 = [1,3], nums2 = [2]
+#     Output: 2.00000
+#     Explanation: merged array = [1,2,3] and median is 2.
+
+# Example 2:
+#     Input: nums1 = [1,2], nums2 = [3,4]
+#     Output: 2.50000
+#     Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+# Constraints:
+#     nums1.length == m
+#     nums2.length == n
+#     0 <= m <= 1000
+#     0 <= n <= 1000
+#     1 <= m + n <= 2000
+#     -106 <= nums1[i], nums2[i] <= 106
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # Combine sorted arrays
+        merged = []
+        i, j = 0, 0
+        n = len(nums1)
+        m = len(nums2)
+
+        while i < n and j < m:
+            if nums1[i] < nums2[j]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+        
+        # Add remaining elements from nums1 and nums2
+        merged.extend(nums1[i:])
+        merged.extend(nums2[j:])
+
+        # Calculate median
+        merged_len = len(merged)
+        if merged_len % 2 == 0:
+            mid = merged_len // 2
+            median = (merged[mid-1] + merged[mid]) / 2
+        else:
+            mid = merged_len // 2
+            median = merged[mid]
+        
+        return median
