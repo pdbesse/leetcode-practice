52a9c20d31f3aaf8fedd9b139e26ab55cc49fdd0
Phillip <phillip.besse@gmail.com>
2023-08-15 08:05:00 -0400

    update WildcardMatching to use backtracking instead of DP

--- top-interview-questions/hard/backtracking.py
+++ top-interview-questions/hard/backtracking.py
@@ -192,21 +192,56 @@ class Solution:
 
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # Create a 2D DP table to store the matching results
-        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
-        dp[0][0] = True  # Empty pattern matches empty string
+        memo = {}  # Memoization dictionary
         
-        # Initialize the first row of DP table
-        for j in range(1, len(p) + 1):
-            if p[j - 1] == '*':
-                dp[0][j] = dp[0][j - 1]
+        def backtrack(s_index, p_index):
+            if (s_index, p_index) in memo:
+                return memo[(s_index, p_index)]
+            
+            if p_index == len(p):
+                return s_index == len(s)
+            
+            if s_index < len(s) and (p[p_index] == '?' or p[p_index] == s[s_index]):
+                result = backtrack(s_index + 1, p_index + 1)
+            elif p[p_index] == '*':
+                result = any(backtrack(i, p_index + 1) for i in range(s_index, len(s) + 1))
+            else:
+                result = False
+            
+            memo[(s_index, p_index)] = result
+            return result
         
-        # Fill in the DP table
-        for i in range(1, len(s) + 1):
-            for j in range(1, len(p) + 1):
-                if p[j - 1] == '?' or s[i - 1] == p[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
-                elif p[j - 1] == '*':
-                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-        
-        return dp[len(s)][len(p)]
+        return backtrack(0, 0)
+
+    
+'''REGULAR EXPRESSION MATCHING'''
+# Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+#     - '.' Matches any single character.​​​​
+#     - '*' Matches zero or more of the preceding element.
+# The matching should cover the entire input string (not partial).
+
+# Example 1:
+#     Input: s = "aa", p = "a"
+#     Output: false
+#     Explanation: "a" does not match the entire string "aa".
+
+# Example 2:
+#     Input: s = "aa", p = "a*"
+#     Output: true
+#     Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+# Example 3:
+#     Input: s = "ab", p = ".*"
+#     Output: true
+#     Explanation: ".*" means "zero or more (*) of any character (.)".
+
+# Constraints:
+#     1 <= s.length <= 20
+#     1 <= p.length <= 20
+#     s contains only lowercase English letters.
+#     p contains only lowercase English letters, '.', and '*'.
+#     It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
