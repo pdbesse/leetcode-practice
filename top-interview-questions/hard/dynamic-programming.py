005e18637edf593784434d132825d2967fd696f8
Phillip <phillip.besse@gmail.com>
2023-08-18 12:14:00 -0400

    add PerfectSquares solution

--- top-interview-questions/hard/dynamic-programming.py
+++ top-interview-questions/hard/dynamic-programming.py
@@ -141,3 +141,38 @@ class Solution:
                 sell[i] = max(sell[i-1], buy[i-1] + prices[i])
             
             return sell[n-1]  # The maximum profit on the last day (sell[n-1]) is the answer.
+
+'''PERFECT SQUARES'''
+# Given an integer n, return the least number of perfect square numbers that sum to n.
+# A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+
+# Example 1:
+#     Input: n = 12
+#     Output: 3
+#     Explanation: 12 = 4 + 4 + 4.
+
+# Example 2:
+#     Input: n = 13
+#     Output: 2
+#     Explanation: 13 = 4 + 9.
+
+# Constraints:
+#     1 <= n <= 104
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # Create a list to store the minimum number of perfect squares needed for each number from 0 to n
+        dp = [0] * (n + 1)
+        
+        # Calculate the minimum number of perfect squares needed for each number
+        for i in range(1, n + 1):
+            # Initialize with the maximum possible value
+            dp[i] = i
+            
+            # Check each perfect square number less than or equal to i
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
+        
+        return dp[n]
