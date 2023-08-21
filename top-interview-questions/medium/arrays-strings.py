003c4865bc4ae701a059d0b38925b818f56b2f61
Phillip <phillip.besse@gmail.com>
2023-08-21 08:03:13 -0400

    add WordBreak solution; update medium/arrays-strings.py

--- top-interview-questions/medium/arrays-strings.py
+++ top-interview-questions/medium/arrays-strings.py
@@ -69,7 +69,7 @@ class Solution:
         return out
 
 '''SET MATRIX ZEROES'''
-# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+# Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's.
 # You must do it in place.
 
 # Example 1:
