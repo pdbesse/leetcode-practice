384faa589aa3bbee70aa1f09c3e5369a575f8be8
Phillip <phillip.besse@gmail.com>
2023-08-21 08:08:57 -0400

    add WordBreakII solution

--- top-interview-questions/hard/dynamic-programming.py
+++ top-interview-questions/hard/dynamic-programming.py
@@ -218,3 +218,49 @@ class Solution:
                     break
         
         return dp[n]
+    
+'''WORD BREAK II'''
+# Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+# Example 1:
+#     Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+#     Output: ["cats and dog","cat sand dog"]
+
+# Example 2:
+#     Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+#     Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+#     Explanation: Note that you are allowed to reuse a dictionary word.
+
+# Example 3:
+#     Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+#     Output: []
+
+# Constraints:
+#     1 <= s.length <= 20
+#     1 <= wordDict.length <= 1000
+#     1 <= wordDict[i].length <= 10
+#     s and wordDict[i] consist of only lowercase English letters.
+#     All the strings of wordDict are unique.
+#     Input is generated in a way that the length of the answer doesn't exceed 105.
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # Create set of valid words for faster lookup
+        wordSet = set(wordDict)
+
+        # DP array to store possible sentences for each index
+        dp = [[] for _ in range(len(s) + 1)]
+        dp[0] = ['']
+
+        for end in range(1, len(s) + 1):
+            for start in range(end):
+                word = s[start:end]
+                if word in wordSet:
+                    for prev_sent in dp[start]:
+                        if prev_sent:
+                            dp[end].append(prev_sent + ' ' + word)
+                        else:
+                            dp[end].append(word)
+        
+        return dp[len(s)]
