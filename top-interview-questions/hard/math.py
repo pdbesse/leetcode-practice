853d6b24a1ec5fc35b81281d4ed077384560c17a
Phillip <phillip.besse@gmail.com>
2023-08-28 08:08:29 -0400

    add MaxPointsOnALine solution

--- top-interview-questions/hard/math.py
+++ top-interview-questions/hard/math.py
@@ -1,5 +1,6 @@
 from typing import List
 from functools import cmp_to_key
+from collections import defaultdict
 
 '''LARGEST NUMBER'''
 # Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
@@ -37,3 +38,45 @@ class Solution:
 
         # If the result is an empty string, return '0'
         return largest_num if largest_num else '0'
+
+'''MAX POINTS ON A LINE'''
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+# Example 1:
+#     Input: points = [[1,1],[2,2],[3,3]]
+#     Output: 3
+
+# Example 2:
+#     Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+#     Output: 4
+
+# Constraints:
+#     1 <= points.length <= 300
+#     points[i].length == 2
+#     -104 <= xi, yi <= 104
+#     All the points are unique.
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) <= 2:
+            return len(points)
+        
+        max_points = 0
+        
+        for i, point1 in enumerate(points):
+            slopes = defaultdict(int)  # To store slopes and their frequencies
+            duplicate = 1  # To count duplicate points with the same (x, y) coordinates
+            
+            for j, point2 in enumerate(points):
+                if i != j:
+                    if point1 == point2:
+                        duplicate += 1
+                    else:
+                        # Calculate the slope and handle vertical lines (infinite slope)
+                        slope = float('inf') if point1[0] == point2[0] else (point2[1] - point1[1]) / (point2[0] - point1[0])
+                        slopes[slope] += 1
+            
+            # Update the maximum number of points on the same line
+            max_points = max(max_points, duplicate + max(slopes.values(), default=0))
+        
+        return max_points
