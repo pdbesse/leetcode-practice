ca4629c563a8b7297eb51bc522210090f9371f29
pdbesse <phillip.besse@gmail.com>
2023-09-04 11:42:21 -0400

    add TheSkylineProblem solution

--- top-interview-questions/hard/others.py
+++ top-interview-questions/hard/others.py
@@ -1,4 +1,5 @@
 from typing import List
+import heapq
 
 '''QUEUE RECONSTRUCTION BY HEIGHT'''
 # You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
@@ -86,3 +87,49 @@ class Solution:
                 right -= 1
         
         return trapped_water
+    
+'''THE SKYLIGHT PROBLEM'''
+# - A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
+# - The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
+#     - lefti is the x coordinate of the left edge of the ith building.
+#     - righti is the x coordinate of the right edge of the ith building.
+#     - heighti is the height of the ith building.
+# - You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+# - The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1],[x2,y2],...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
+# Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]
+# Example 1:
+#     Input: buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
+#     Output: [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
+#     Explanation:
+#         Figure A shows the buildings of the input.
+#         Figure B shows the skyline formed by those buildings. The red points in figure B represent the key points in the output list.
+# Example 2:
+#     Input: buildings = [[0,2,3],[2,5,3]]
+#     Output: [[0,3],[5,0]]
+# Constraints:
+#     1 <= buildings.length <= 104
+#     0 <= lefti < righti <= 231 - 1
+#     1 <= heighti <= 231 - 1
+#     buildings is sorted by lefti in non-decreasing order.
+
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        # Convert buildings into events
+        events = [(L, -H, R) for L, R, H in buildings] + [(R, 0, 0) for _, R, _ in buildings]
+        
+        # Sort events
+        events.sort()
+        
+        # Result and current heights
+        res = [[0, 0]]
+        hp = [(0, float('inf'))]
+        
+        for x, negH, R in events:
+            while hp[0][1] <= x:
+                heapq.heappop(hp)
+            if negH:
+                heapq.heappush(hp, (negH, R))
+            if res[-1][1] != -hp[0][0]:
+                res.append([x, -hp[0][0]])
+                
+        return res[1:]
