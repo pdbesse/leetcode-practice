1e32df86f3bddf2294092ef28aa5b0f727f7079c
Phillip <phillip.besse@gmail.com>
2023-09-06 10:20:59 -0400

    add LargestRectangleInAHistogram solution

--- top-interview-questions/hard/others.py
+++ top-interview-questions/hard/others.py
@@ -86,3 +86,41 @@ class Solution:
                 right -= 1
         
         return trapped_water
+
+'''LARGEST RECTANGLE IN A HISTOGRAM'''
+# Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+# Example 1:
+# Input: heights = [2,1,5,6,2,3]
+# Output: 10
+# Explanation: 
+#     The above is a histogram where width of each bar is 1.
+#     The largest rectangle is shown in the red area, which has an area = 10 units.
+
+# Example 2:
+#     Input: heights = [2,4]
+#     Output: 4
+
+# Constraints:
+#     1 <= heights.length <= 105
+#     0 <= heights[i] <= 104
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []  # Stack to store the indices of the bars
+        max_area = 0
+
+        for i, h in enumerate(heights):
+            while stack and h < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+
+        # Calculate the remaining rectangles in the stack
+        while stack:
+            height = heights[stack.pop()]
+            width = len(heights) if not stack else len(heights) - stack[-1] - 1
+            max_area = max(max_area, height * width)
+
+        return max_area
