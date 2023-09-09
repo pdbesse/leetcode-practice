5e386cf1a4615f376da99262395e3d08ae57e7c5
Phillip <phillip.besse@gmail.com>
2023-09-09 12:23:40 -0400

    update hard/others.py

--- top-interview-questions/hard/others.py
+++ top-interview-questions/hard/others.py
@@ -88,7 +88,7 @@ class Solution:
         
         return trapped_water
     
-'''THE SKYLIGHT PROBLEM'''
+'''THE SKYLINE PROBLEM'''
 # - A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
 # - The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
 #     - lefti is the x coordinate of the left edge of the ith building.
