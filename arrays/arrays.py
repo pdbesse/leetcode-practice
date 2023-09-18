24804651c19bed61563a94171b40830ef4875121
Phillip <phillip.besse@gmail.com>
2023-09-18 08:30:07 -0400

    add CombinationSumII solution

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -246,3 +246,58 @@ class Solution:
                         right -= 1
         
         return result
+
+'''COMBINATION SUM II'''
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+# Each number in candidates may only be used once in the combination.
+# Note: The solution set must not contain duplicate combinations.
+
+
+# Example 1:
+#     Input: candidates = [10,1,2,7,6,1,5], target = 8
+#     Output: 
+#         [
+#         [1,1,6],
+#         [1,2,5],
+#         [1,7],
+#         [2,6]
+#         ]
+
+# Example 2:
+#     Input: candidates = [2,5,2,1,2], target = 5
+#     Output: 
+#         [
+#         [1,2,2],
+#         [5]
+#         ]
+
+# Constraints:
+#     1 <= candidates.length <= 100
+#     1 <= candidates[i] <= 50
+#     1 <= target <= 30
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, end, path):
+            if target == 0:
+                # If target is zero, we have our combination
+                result.append(path)
+                return
+            if target < 0 or start == len(candidates):
+                # If target is negative or we have exhausted candidates
+                return
+            
+            for i in range(start, len(candidates)):
+                # Skip duplicates to avoid duplicating combinations
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                # Recursively try adding current candidate to path
+                backtrack(i + 1, target - candidates[i], path + [candidates[i]])
+        
+        # Sort candidates to handle duplicates efficiently
+        candidates.sort()
+        result = []
+        backtrack(0, target, [])
+
+        return result
